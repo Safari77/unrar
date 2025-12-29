2ecc83dab8101b03900367624c99c1aa25626826
Sami Farin <hvtaifwkbgefbaei@gmail.com>
2025-12-29 16:10:46 +0200

    original 7.2.2+7.2.3
    b3sum:
    fb99ea4919a664067cba13fb8ad89095b5c1e5130f71ff1dbe9a1d13f782579e  unrarsrc-7.2.2.tar.gz
    6ca293e04039288f17086d43782431371724ac8cf96f653a559857e688bdd084  unrarsrc-7.2.3.tar.gz

--- version.hpp
+++ version.hpp
@@ -1,6 +1,6 @@
 #define RARVER_MAJOR     7
 #define RARVER_MINOR    20
-#define RARVER_BETA      1
-#define RARVER_DAY      28
-#define RARVER_MONTH    10
+#define RARVER_BETA      3
+#define RARVER_DAY      18
+#define RARVER_MONTH    12
 #define RARVER_YEAR   2025
